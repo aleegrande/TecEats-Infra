c7cb2b8567ef05c8e3903bc3438eb7dd29c0b164
Alejandra Grande <gaalgrandede@ittepic.edu.mx>
2019-12-09 14:56:01 -0700

    auth

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { AngularFireAuth } from "@angular/fire/auth";
+import { User } from "../models/user.model";
+import { auth } from "firebase/app";
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  public isLogged: any = false;
+
+
+  constructor(public afAuth: AngularFireAuth, private afsAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(user => (this.isLogged = user));
+  }
+  //registro
+  async onRegister(user: User) {
+    try {
+      return await this.afAuth.auth.createUserWithEmailAndPassword(
+        user.email,
+        user.password)
+
+    } catch (error) {
+      console.log("error con el egistro", error);
+    }
+  }
+
+
+  //login
+  async onLogin(user: User) {
+    try {
+      return await this.afAuth.auth.signInWithEmailAndPassword(
+        user.email,
+        user.password
+      )
+
+    } catch (error) {
+      console.log("Error en el logeo", error);
+
+    }
+  }
+  loginGitUser(){
+    return this.afAuth.auth.signInWithPopup(new auth.GithubAuthProvider());
+  }
+  loginGoogleUser(){
+    return this.afsAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  loginFacebook(){
+   return this.afsAuth.auth.signInWithPopup(new auth.FacebookAuthProvider());
+ }
+ 
+ loginTwiter(){
+   return this.afsAuth.auth.signInWithPopup(new auth.TwitterAuthProvider());
+ }
+}
