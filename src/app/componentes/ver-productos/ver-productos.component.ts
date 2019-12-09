bb5d37a76c2425af813354dbda2f962a6d967b23
Stratos1234 <rimalopezpe@ittepic.edu.mx>
2019-12-08 19:12:44 -0700

    Ya funciona

--- src/app/componentes/ver-productos/ver-productos.component.ts
+++ src/app/componentes/ver-productos/ver-productos.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { ProductosService } from '../../services/productos.service';
+import { Productos } from 'src/app/models/productos';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { finalize, take } from 'rxjs/operators'
+import { Observable } from 'rxjs';
+import { async } from 'q';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-ver-productos',
   templateUrl: './ver-productos.component.html',
   styleUrls: ['./ver-productos.component.css']
 })
 export class VerProductosComponent implements OnInit {
+  productos = [];
 
-  constructor() { }
+  constructor(public productService: ProductosService) { }
 
   ngOnInit() {
+    this.productService.getProducto().subscribe(productos => {
+      this.productos = productos;
+    });
   }
 
 }
